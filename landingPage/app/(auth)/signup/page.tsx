38012c4dd1dd776879b7b5699347062be5362439
Kuchnahi@gmail.com <chiragagrawal774@gmail.com>
2024-09-23 03:50:47 +0530

    added calendly integration

--- landingPage/app/(auth)/signup/page.tsx
+++ landingPage/app/(auth)/signup/page.tsx
@@ -1,11 +1,19 @@
-export const metadata = {
-  title: "Sign Up - Open PRO",
-  description: "Page description",
-};
+"use client"; // This makes the component a Client Component
 
-import Link from "next/link";
+
+// export const metadata = {
+//   title: "Sign Up - Open PRO",
+//   description: "Page description",
+// };
+
+import { useForm, ValidationError } from '@formspree/react';
 
 export default function SignUp() {
+  const [state, handleSubmit] = useForm("xrbzwknp"); // Replace with your Formspree form ID
+
+  if (state.succeeded) {
+    return <p>Thanks for joining the waitlist!</p>;
+  }
   return (
     <section>
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
@@ -13,11 +21,11 @@ export default function SignUp() {
           {/* Section header */}
           <div className="pb-12 text-center">
             <h1 className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text font-nacelle text-3xl font-semibold text-transparent md:text-4xl">
-              Create an account
+              Join our waitlist and get early access ! 
             </h1>
           </div>
           {/* Contact form */}
-          <form className="mx-auto max-w-[400px]">
+          <form onSubmit={handleSubmit} className="mx-auto max-w-[400px]">
             <div className="space-y-5">
               <div>
                 <label
@@ -37,21 +45,6 @@ export default function SignUp() {
               <div>
                 <label
                   className="mb-1 block text-sm font-medium text-indigo-200/65"
-                  htmlFor="name"
-                >
-                  Company Name <span className="text-red-500">*</span>
-                </label>
-                <input
-                  id="company"
-                  type="text"
-                  className="form-input w-full"
-                  placeholder="Your company name"
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  className="mb-1 block text-sm font-medium text-indigo-200/65"
                   htmlFor="email"
                 >
                   Work Email <span className="text-red-500">*</span>
@@ -65,6 +58,21 @@ export default function SignUp() {
               </div>
               <div>
                 <label
+                  className="mb-1 block text-sm font-medium text-indigo-200/65"
+                  htmlFor="name"
+                >
+                  Company Name <span className="text-red-500">*</span>
+                </label>
+                <input
+                  id="company"
+                  type="text"
+                  className="form-input w-full"
+                  placeholder="Your company name"
+                  required
+                />
+              </div>
+              {/* <div>
+                <label
                   className="block text-sm font-medium text-indigo-200/65"
                   htmlFor="password"
                 >
@@ -76,27 +84,32 @@ export default function SignUp() {
                   className="form-input w-full"
                   placeholder="Password (at least 10 characters)"
                 />
-              </div>
+              </div> */}
             </div>
             <div className="mt-6 space-y-5">
               <button className="btn w-full bg-gradient-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%]">
-                Register
+                Register for Waitlist
               </button>
               <div className="flex items-center gap-3 text-center text-sm italic text-gray-600 before:h-px before:flex-1 before:bg-gradient-to-r before:from-transparent before:via-gray-400/25 after:h-px after:flex-1 after:bg-gradient-to-r after:from-transparent after:via-gray-400/25">
                 or
               </div>
-              <button className="btn relative w-full bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%]">
-                Sign In with Google
+              <a href="https://calendly.com/chiragagrawal774/30min"
+                    target="_blank"
+                    rel="noopener noreferrer">
+              <button type="button" className="btn relative w-full bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%]">
+                Schedule a Demo Meet
               </button>
+              </a>
             </div>
           </form>
-          {/* Bottom link */}
+          {/* Bottom link 
           <div className="mt-6 text-center text-sm text-indigo-200/65">
             Already have an account?{" "}
             <Link className="font-medium text-indigo-500" href="/signin">
               Sign in
             </Link>
           </div>
+          */}
         </div>
       </div>
     </section>
